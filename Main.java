3640e481b2d5ae322749fbdbc2adac0139749acb
MattNorris08 <mattnorris08@icloud.com>
2023-01-28 13:09:32 -0700

    Variable Practice

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        var familyMembers = ("Mom, Dad, Jack, Bailey, Stormy");
+        System.out.println(familyMembers);
+        if(familyMembers == "Mom, Dad, Jack, Bailey, Stormy"){
+            System.out.print("I love my family!");
+        } else{
+            System.out.print("Where is everyone?");
+        }
+    }
 }
