1ed24fd187cb84f49c691aac112cf998783e5bd9
MattNorris08 <mattnorris08@icloud.com>
2023-01-28 15:34:15 -0700

    Learning about variable switching and scanners

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        //learning about variables
+
         var familyMembers = ("Mom, Dad, Jack, Bailey, Stormy");
         System.out.println("My family is " + familyMembers);
         if(familyMembers == "Mom, Dad, Jack, Bailey, Stormy"){
@@ -18,5 +23,36 @@ public class Main {
         System.out.println(symbol);
 
         String name = "Matt, Jack";
+
+        //variable switching
+
+        String x = "Cat";
+        String y = "Dog";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        //learning about scanners
+
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("What is your name");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName);
+
+        System.out.println("How old are you?");
+        int userAge = scanner.nextInt();
+        System.out.println("You are " + userAge + " Years old");
+        scanner.nextLine();
+
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
+        System.out.println(food + " is a good food");
+
     }
 }
