90ba3bbafeb33a61135b0f08aabf577665246106
MattNorris08 <mattnorris08@icloud.com>
2023-01-28 15:15:00 -0700

    More variable work

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         var familyMembers = ("Mom, Dad, Jack, Bailey, Stormy");
-        System.out.println(familyMembers);
+        System.out.println("My family is " + familyMembers);
         if(familyMembers == "Mom, Dad, Jack, Bailey, Stormy"){
-            System.out.print("I love my family!");
+            System.out.println("I love my family!");
         } else{
-            System.out.print("Where is everyone?");
+            System.out.println("Where is everyone?");
         }
+
+        int myAge = 14;
+        System.out.println("And I am " + myAge + " years old!");
+
+        boolean amIAnAdult = myAge >= 18;
+        System.out.println(amIAnAdult);
+
+        char symbol = '@';
+        System.out.println(symbol);
+
+        String name = "Matt, Jack";
     }
 }
